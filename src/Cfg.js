c273921879e6b65c82c819a1ab47654390dbf966
sapryx <sapryxv@gmail.com>
2026-08-12 14:33:17 +0300

    refactor player movement

---
+++ src/Cfg.js
@@ -0,0 +1,4 @@
+const Cfg = Object.freeze({
+    TileSize: 16,
+    MovementSpeed: 0.15
+});
--- src/gameLoop.js
+++ src/gameLoop.js
@@ -21,26 +21,22 @@ function handleInput() {
     movePlayer(movementInputRaw);
 }
 
-let walkAnim = null;
-
 function movePlayer(movementInput) {
-    const scale = 16;
-    const walkSpeed = 0.15;
-
     G.turnManager.isPlayerTurn = false;
 
     G.playerView.__x = G.player.pos.x;
     G.playerView.__y = -G.player.pos.y;
 
-    G.player.moveHorizontal(movementInput.x * scale);
-    G.player.moveVertical(movementInput.y * scale);
+    G.player.moveHorizontal(movementInput.x * Cfg.TileSize);
+    G.player.moveVertical(movementInput.y * Cfg.TileSize);
 
-    walkAnim = anim(G.playerView, {
+    killAnim(G.playerView);
+
+    anim(G.playerView, {
         __x: G.player.pos.x,
         __y: -G.player.pos.y
-    }, walkSpeed);
-
-    _setTimeout(() => {
-        G.turnManager.isPlayerTurn = true;
-    }, walkSpeed);
+    }, Cfg.MovementSpeed)
+        .__setOnComplete(() => {
+            G.turnManager.isPlayerTurn = true;
+        });
 }
