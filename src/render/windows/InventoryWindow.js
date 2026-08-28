d173ae12f0b79b2bac0720f9ca5481bdc94ab36e
sapryx <sapryxv@gmail.com>
2026-08-28 13:30:51 +0300

    update inventory window rendering

--- src/render/windows/InventoryWindow.js
+++ src/render/windows/InventoryWindow.js
@@ -6,10 +6,25 @@ class InventoryWindow extends GameWindow {
 
     open() {
         super.open();
-        const itemSidebar = this._node.__findChild(node => node.__userData && node.__userData.type === "item_sidebar");
-        const inventory = G.player.inventory;
+        const items = G.player.inventory.getItems();
+        const itemCount = G.player.inventory.itemCount;
 
-        for(const item of inventory.getItems()) {
+        if(itemCount === 0) {
+            return;
+        }
+
+        this._fillItemSidebar(items);
+        this._updateItemPanel(items);
+    }
+
+    /**
+     * @param {ReadonlyArray<ItemInstance>} items
+     * @private
+     */
+    _fillItemSidebar(items) {
+        const itemSidebar = this._node.__findChild(node => node.__userData && node.__userData.type === "item_sidebar");
+
+        for(const item of items) {
             if(item == null) {
                 continue;
             }
@@ -18,5 +33,19 @@ class InventoryWindow extends GameWindow {
             itemCard.init();
             itemCard.set(item.item);
         }
+    }
+
+    /**
+     * @param {ReadonlyArray<ItemInstance>} items
+     * @private
+     */
+    _updateItemPanel(items) {
+        const itemPanel = this._node.__findChild(node => node.__userData && node.__userData.type === "item_panel");
+        const nameNode = itemPanel.__findChild(node => node.__userData && node.__userData.type === "name");
+        const descriptionNode = itemPanel.__findChild(node => node.__userData && node.__userData.type === "description");
+        const firstItem = items[0];
+
+        nameNode.__text = firstItem.item.name;
+        descriptionNode.__text = firstItem.item.description;
     }
 }
